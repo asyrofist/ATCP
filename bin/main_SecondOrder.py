864a2ca85ff6a91676ec78646e6f70ef9dc31915
Roger Granada <rgranada2004@gmail.com>
2011-12-08 16:27:45 -0200

    Finished method to build the thesaurus

--- bin/main_SecondOrder.py
+++ bin/main_SecondOrder.py
@@ -16,10 +16,12 @@ def main(type_atc, argv):
 	input_folder = parameters.getInputFolder()
 	language = parameters.getLanguage()
 	min_word_size = parameters.getMinWordSize()
-	max_qty_terms = parameters.getMaxQtyTerms()
+	max_qty_terms = int(parameters.getMaxQtyTerms())
 	output_folder = parameters.getOutputFolder()
 	temp_folder = parameters.getTempFolder()
 	seeds_file = parameters.getSeedsFile()
+	sim_measure = parameters.getSimilarityMeasure() 
+	thesaurus = Thesaurus(output_folder+'T_'+type_atc+'.xml',max_qty_terms)
 
 	ling_corpus = StanfordSyntacticContexts(input_folder, temp_folder, min_word_size)
 	ling_corpus.writeDicAN(temp_folder+'AN_Relations.txt')
@@ -29,9 +31,10 @@ def main(type_atc, argv):
 	print 'Merging terms to Relations2ndOrder.txt'
 	command = "cat "+temp_folder+'AN_Relations.txt '+temp_folder+'SV_Relations.txt '+temp_folder+'VO_Relations.txt '+' > '+temp_folder+'Relations2ndOrder.txt'
 	os.system(command)
-	
+
 	measures = Measures(temp_folder+'Relations2ndOrder.txt', seeds_file)
-	measures.getTopNJaccardToSeed('customer_information', max_qty_terms)
+	dic_topn = measures.getTopNToAllSeeds(sim_measure, max_qty_terms)
+	thesaurus.write(dic_topn)
 
 if __name__ == "__main__":
    main('SecondOrder', sys.argv[1:])
