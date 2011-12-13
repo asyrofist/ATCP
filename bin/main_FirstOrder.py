eb68025a73f62b5426598fdc8852d4623b451d9a
Roger Granada <rgranada2004@gmail.com>
2011-12-13 19:07:54 -0200

    Added logfile recording

--- bin/main_FirstOrder.py
+++ bin/main_FirstOrder.py
@@ -1,31 +1,46 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-import sys, re, os
+import sys, os, time, datetime
 
-from collections import defaultdict
 from StatisticalCorpus import StatisticalCorpus
 from Parameters import Parameters
 from Thesaurus import Thesaurus
-from Miscelaneous import bcolors
-from Miscelaneous import Miscelaneous
+from Miscelaneous import LogFile
 from Measures import MutualInformation
 from Measures import Measures
 
 def main(type_atc, argv):
+	start = time.clock()
+	date_now = datetime.datetime.now()
+	date_now = date_now.strftime("%Y-%m-%d %H:%M:%S")
+
 	parameters = Parameters(type_atc, argv)
 	input_folder = parameters.getInputFolder()
 	language = parameters.getLanguage()
 	min_word_size = int(parameters.getMinWordSize())
-	max_qty_terms = parameters.getMaxQtyTerms()
+	max_qty_terms = int(parameters.getMaxQtyTerms())
 	mi_precision = parameters.getMIPrecision()
 	output_folder = parameters.getOutputFolder()
 	window_size = parameters.getWindowSize()
 	temp_folder = parameters.getTempFolder()
+	record_log = parameters.getRecordLog()
 	seeds_file = parameters.getSeedsFile()
-	sim_measure = parameters.getSimilarityMeasure() 
-	misc = Miscelaneous()
+	sim_measure = parameters.getSimilarityMeasure()
+	del parameters
+ 
+	if record_log:
+		logfile = LogFile(str(date_now), None, input_folder, language, min_word_size, max_qty_terms, mi_precision, output_folder, window_size, temp_folder, seeds_file, sim_measure)
+		logfile.writeLogfile('- Building statistical corpus at '+temp_folder+'\n')
+	else:
+		print '- Building statistical corpus at '+temp_folder
+
 	stat_corpus = StatisticalCorpus(input_folder, temp_folder, min_word_size, window_size)
+
+	if record_log:
+		logfile.writeLogfile('- Choosen "'+language+'" as the main language\n')
+	else:
+		print '- Choosen "'+language+'" as the main language'
 	
 	if language == 'pt':
 		stat_corpus.buildCorpus_pt()	
@@ -38,9 +53,18 @@ def main(type_atc, argv):
 		Uses count.pl from NGram Statistical Package (NSP) to get Bigrams in a window
 	"""
 
-	print 'Getting bigrams to W'+window_size+'_Statistical_corpus.txt'
+	if record_log:
+		logfile.writeLogfile('- Getting bigrams to W'+window_size+'_Statistical_corpus.txt \n')
+	else:
+		print '- Getting bigrams to W'+window_size+'_Statistical_corpus.txt'
+
 	command = 'count.pl --ngram 2 '+param_nsp+' --window '+window_size+' '+temp_folder+'W'+window_size+'_Statistical_corpus.txt '+temp_folder+'Statistical_corpus.txt'
 	os.system(command)
+
+	if record_log:
+		logfile.writeLogfile('- Using '+sim_measure+' as similarity measure \n')
+	else:
+		print '- Using '+sim_measure+' as similarity measure'
 
 	if sim_measure == 'mutual_information':
 		mi = MutualInformation(temp_folder, 'W'+window_size+'_Statistical_corpus.txt', seeds_file, mi_precision)
@@ -50,8 +74,25 @@ def main(type_atc, argv):
 		measures = Measures(temp_folder+'W'+window_size+'_Relations.txt', seeds_file)
 		dic_terms = measures.getTopNToAllSeeds(sim_measure, max_qty_terms)
 
+	del stat_corpus
+
+	if record_log:
+		logfile.writeLogfile('- Building thesaurus in '+output_folder+'T'+window_size+'_'+type_atc+'_'+sim_measure+'.xml \n')
+	else:
+		print '- Building thesaurus in '+output_folder+'T'+window_size+'_'+type_atc+'_'+sim_measure+'.xml'
+
 	thesaurus = Thesaurus(output_folder+'T'+window_size+'_'+type_atc+'_'+sim_measure+'.xml',max_qty_terms)
 	thesaurus.write(dic_terms)
+
+	del thesaurus
+
+	end = time.clock()
+	if record_log:
+		logfile.writeLogfile('- Thesaurus sucessfully built!\nTime consuming: '+str(end - start)+' seconds\n\n')
+	else:
+		print '- Thesaurus sucessfully built!'
+
+	del logfile
 	
 if __name__ == "__main__":
    main('FirstOrder', sys.argv[1:])
